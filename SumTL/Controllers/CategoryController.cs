e80634f5ccb15cb5e574242b7e2f283ea6d1495b
Saikat Mahmud <saikat8486@gmail.com>
2024-05-16 04:13:07 +0600

    ajax call, cat list

--- SumTL/Controllers/CategoryController.cs
+++ SumTL/Controllers/CategoryController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using SumTL.BLL.DTOs;
 using SumTL.BLL.ServiceAccess;
 using SumTL.BLL.Services;
+using System.Text.Json;
 
 namespace SumTL.Controllers
 {
@@ -33,58 +35,71 @@ namespace SumTL.Controllers
         //    var i = 0;
         //    return View();
         //}
+        //#region MVC
+        //public IActionResult GetAll()
+        //{
+        //    var data = categoryService.Get();
+        //    return View(data);
+        //}
+        //public IActionResult Get(int id)
+        //{
+        //    var data = categoryService.Get(c => c.Id == id);
+        //    return View(data);
+        //}
+        //[HttpGet]
+        //public IActionResult Edit(int id)
+        //{
+        //    var data = categoryService.Get(c => c.Id == id);
+        //    return View(data);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(CategoryDTO ct)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View();
+        //    }
+        //    var result = categoryService.Update(ct);
+        //    TempData["msg"] = result ? "Updated successfully!" : "Error! Not Updated";
+        //    return !result ? View() : RedirectToAction("GetAll");
+        //}
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult Create(CategoryDTO ct)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View();
+        //    }
+        //    var result = categoryService.Create(ct);
+        //    TempData["msg"] = result ? "Added successfully!" : "Error! Could not added";
+        //    return !result ? View() : RedirectToAction("GetAll");
+        //}
 
-        public IActionResult GetAll() 
-        { 
-            var data = categoryService.Get();
-            return View(data);
-        }
-        public IActionResult Get(int id)
-        {
-            var data = categoryService.Get(c => c.Id == id);
-            return View(data);
-        }
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var data = categoryService.Get(c => c.Id == id);
-            return View(data);
-        }
-        [HttpPost]
-        public IActionResult Edit(CategoryDTO ct)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
-            var result = categoryService.Update(ct);
-            TempData["msg"] = result ? "Updated successfully!" : "Error! Not Updated";
-            return !result ? View() : RedirectToAction("GetAll");
-        }
-        [HttpGet]
-        public IActionResult Create()
+        //public IActionResult Delete(int id)
+        //{
+        //    var result = categoryService.Delete(id);
+        //    TempData["msg"] = result ? "Deleted successfully!" : "Error! Could not deleted";
+        //    return !result ? View("ErrorView") : RedirectToAction("GetAll");
+        //}
+        //#endregion
+
+        #region API
+        public IActionResult Index()
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult Create(CategoryDTO ct)
+        public JsonResult GetAll()
         {
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
-            var result = categoryService.Create(ct);
-            TempData["msg"] = result ? "Added successfully!" : "Error! Could not added";
-            return !result ? View() : RedirectToAction("GetAll");
+            var data = categoryService.Get();
+            return Json(new { result = data });
         }
+        
 
-        public IActionResult Delete(int id)
-        {
-            var result = categoryService.Delete(id);
-            TempData["msg"] = result ? "Deleted successfully!" : "Error! Could not deleted";
-            return !result ? View("ErrorView") : RedirectToAction("GetAll");
-        }
-
-
+        #endregion
     }
 }
---
+++ SumTL/wwwroot/js/getCategoryList.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    LoadList();
+});
+
+
+const renderList = (obj) => {
+    const template = $("#category_list").html();
+    const compiledTemplate = Handlebars.compile(template);
+    debugger;
+    const html = compiledTemplate({ rows: obj });
+    $(".render_data").html(html);
+};
+
+var LoadList = function () {
+    console.log("executed");
+    $.ajax({
+        url: '/Category/GetAll',
+        type: 'get',
+        success: function (data) {
+            debugger;
+            if (data.result && data.result.length > 0) {
+                renderList(data.result);
+            } else {
+                $(".render_data").html("Category List is empty!");
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
