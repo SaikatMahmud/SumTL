98b4fcea1e9ceb79f92611d3b943f9b7835d253b
Saikat Mahmud <saikat8486@gmail.com>
2024-05-23 03:46:56 +0600

    item bulk sp

--- SumTL.DAL/Repos/ItemRepo.cs
+++ SumTL.DAL/Repos/ItemRepo.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using SumTL.DAL.Interfaces;
 using SumTL.DAL.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -20,9 +23,45 @@ namespace SumTL.DAL.Repos
 
         public async Task<(bool success, string? error)> UploadBulk(List<Item> items)
         {
-            await _db.Items.AddRangeAsync(items);
-            await _db.SaveChangesAsync();          
-            return (true, null);
+            //Add items to the database directly using EF
+            //await _db.Items.AddRangeAsync(items);
+            //await _db.SaveChangesAsync();          
+            //return (true, null);
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(items);
+                DataTable dt = JsonConvert.DeserializeObject<DataTable>(json);
+                dt.Columns.Remove("Id");
+                dt.Columns.Remove("Category");
+                dt.Columns.Remove("Images");
+
+                //var rowCount = new SqlParameter
+                //{
+                //    ParameterName = "@insertCount",
+                //    SqlDbType = SqlDbType.Int,
+                //    Direction = ParameterDirection.Output,
+                //};
+
+                var list = new SqlParameter("@ItemList", SqlDbType.Structured)
+                {
+                    TypeName = "ItemType",
+                    Value = dt
+                };
+
+               // var result = _db.Database.ExecuteSqlRaw("spInsertItems @ItemList", table);
+                var result = _db.Database.ExecuteSqlRaw("spInsertItems {0}", list);
+                //int overallCount = (int)rowCount.Value;
+                if(result != items.Count)
+                {
+                    return (true, "Some items have dublicate names and excluded from the list.");
+                }
+                return (true, "All items insterted");
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
         }
     }
 }
